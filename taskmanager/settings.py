6930f321be1221a4842df9ac255bc49b261f9549
Bolliboinapavansai <pavansai.bolliboina@gmail.com>
2024-07-17 15:33:42 -0400

    whitenoise setting

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,25 +75,26 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "NAME": env("DJANGO_DB_NAME"),
+            "USER": env("DJANGO_DB_USER"),
+            "PASSWORD":env("DJANGO_DB_USER_PSWD"),
+            "HOST":env("DJANGO_HOST"),
+            "PORT":env("DJANGO_PORT"),
+        },
     }
 }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "OPTIONS": {
-#             "NAME": "postgres",
-#             "USER": "postgres",
-#             "PASSWORD":"Dell@007",
-#             "HOST":"localhost",
-#             "PORT":"5432",
-#         },
-#     }
-# }
 
 
 # Password validation
