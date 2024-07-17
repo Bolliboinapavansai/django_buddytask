39512ca0a237d3d2338cd9926796d851686eebbd
Bolliboinapavansai <pavansai.bolliboina@gmail.com>
2024-07-17 16:01:26 -0400

    domain updates

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -20,7 +20,9 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1", ""]
+ALLOWED_HOSTS = ["localhost","127.0.0.1", "buddytask.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS=["buddytask.up.railway.app"]
 
 
 # Application definition
